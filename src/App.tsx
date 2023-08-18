15858c2e4c0b6b403bf0015a715926dd9102d7c1
Dikshant Pandey <pdikshant242@gmail.com>
2023-08-19 03:10:12 +0530

    fetch API data completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,39 @@ import {
   Route,
   // Navigate,
 } from "react-router-dom";
+import { useEffect, useState } from "react";
 import SignIn from "./pages/SignIn";
 import Navbar from "./components/Navbar";
 import Second from "./pages/Second";
 
+interface Item {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 function App() {
-  const data: string | null = localStorage.getItem("formData");
+  const [data, setData] = useState<Item[] | null>(null);
+  useEffect(() => {
+    // Replace this with your actual API fetching logic
+    // For demonstration purposes, we're using a mock API call here
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          " https://jsonplaceholder.typicode.com/posts"
+        );
+        const responseData = await response.json();
+        // Slice the data to only the first 5 items
+        const slicedData = responseData.slice(0, 5);
+        setData(slicedData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  // const checkData: string | null = localStorage.getItem("formData");
   console.log("app", data);
   return (
     <>
@@ -18,7 +45,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<SignIn />} />
-          <Route path="/second" element={<Second />} />
+          <Route path="/second" element={<Second data={data} />} />
         </Routes>
       </Router>
     </>
--- src/pages/Second.tsx
+++ src/pages/Second.tsx
@@ -1,9 +1,25 @@
-import { Navigate } from "react-router-dom";
-export default function Second() {
-  const data: string | null = localStorage.getItem("formData");
-  if (data === null) {
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+interface Item {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface SecondProps {
+  data: Item[] | null;
+}
+
+const Second: React.FC<SecondProps> = ({ data }) => {
+  console.log(data);
+  const cdata: string | null = localStorage.getItem('formData');
+  if (cdata === null) {
     return <Navigate to="/" />;
   }
 
-  return <>Hello 2nd pages;</>;
-}
+  return <div>Hello 2nd page;</div>;
+};
+
+export default Second;
