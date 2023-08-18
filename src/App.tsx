d82873878cbde63d2cb7d0ffdcad2d58e4e55741
Dikshant Pandey <pdikshant242@gmail.com>
2023-08-19 01:40:27 +0530

    Complted Login Page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import SignIn from './pages/SignIn'
+
+function App() {
+
+  return (
+    <>
+    <SignIn/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,110 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  const [formData, setFormData] = React.useState({
+    name: '',
+    email: '',
+    number: '',
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const formDataObject = {
+      name: data.get('name'),
+      email: data.get('email'),
+      number: data.get('number'),
+    };
+
+    // Store form data in local storage
+    localStorage.setItem('formData', JSON.stringify(formDataObject));
+
+    // Reset the form values
+    setFormData({
+      name: '',
+      email: '',
+      number: '',
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Your name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+              value={formData.name}
+              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={formData.email}
+              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="number"
+              label="Phone number"
+              type="number"
+              id="number"
+              autoComplete="number"
+              value={formData.number}
+              onChange={(e) => setFormData({ ...formData, number: e.target.value })}
+            />
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
